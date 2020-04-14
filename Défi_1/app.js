6ce7f4b0d66bc768c3077118a1d315e046821a7b
meach <meachkaddour1@gmail.com>
2020-04-14 17:42:27 +0100

    added solution défi 1

--- Défi_1/app.js
+++ Défi_1/app.js
@@ -1,1 +1,12 @@
-let btn =
+let btn = document.querySelector('button');
+let outData = document.getElementById('output');
+btn.addEventListener('click', loadData); // création d'un event listener
+// function loadData
+function loadData() {
+    const XHR = new XMLHttpRequest; // objet XMLHttpRequest
+    XHR.open('GET', 'data.txt'); // chargement des donnés a partirfichier data.txt
+    XHR.send(); //send request
+    XHR.responseType = 'text';
+    XHR.onload = () => { outData.textContent = XHR.response }; //Insertion des donnés à l'interieur de la balise `output`.
+    console.log(XHR.readyState);
+}
